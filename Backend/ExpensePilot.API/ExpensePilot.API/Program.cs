2332e8fb861813bb9d05448b5fd85e51b0be5c25
Nouman Saeed <snouman469@gmail.com>
2026-07-21 18:33:20 +0500

    Setup database with Entity Framework migrations

--- Backend/ExpensePilot.API/ExpensePilot.API/Program.cs
+++ Backend/ExpensePilot.API/ExpensePilot.API/Program.cs
@@ -1,3 +1,8 @@
+using ExpensePilot.API.Data;
+using ExpensePilot.API.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")
+    ));
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>();
 
 var app = builder.Build();
 
