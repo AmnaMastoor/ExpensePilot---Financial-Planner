4d59ba9f4109a93a6e7eedcf1e07a1de9d7b5dc2
Nouman Saeed <snouman469@gmail.com>
2026-07-21 18:50:46 +0500

    Implement user registration API

---
+++ Backend/ExpensePilot.API/ExpensePilot.API/Controllers/AuthController.cs
@@ -0,0 +1,60 @@
+﻿using ExpensePilot.API.DTOs;
+using ExpensePilot.API.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExpensePilot.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public AuthController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegisterDto request)
+        {
+            var existingUser = await _userManager.FindByEmailAsync(request.Email);
+
+            if (existingUser != null)
+            {
+                return BadRequest(new
+                {
+                    message = "Email already exists"
+                });
+            }
+
+
+            var user = new ApplicationUser
+            {
+                UserName = request.Email,
+                Email = request.Email,
+                FullName = request.FullName
+            };
+
+
+            var result = await _userManager.CreateAsync(
+                user,
+                request.Password
+            );
+
+
+            if (!result.Succeeded)
+            {
+                return BadRequest(result.Errors);
+            }
+
+
+            return Ok(new
+            {
+                message = "User registered successfully"
+            });
+        }
+    }
+}
