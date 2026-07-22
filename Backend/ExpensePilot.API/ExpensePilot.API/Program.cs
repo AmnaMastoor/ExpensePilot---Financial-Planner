9e7fdaf584206a15e96057cd4a51cd6c6d0ed24c
Amna Mastoor <amnamastoor04@gmail.com>
2026-07-22 21:58:07 +0500

    Migrate database from SQL Server to PostgreSQL

--- Backend/ExpensePilot.API/ExpensePilot.API/Program.cs
+++ Backend/ExpensePilot.API/ExpensePilot.API/Program.cs
@@ -44,9 +44,9 @@ builder.Services.AddSwaggerGen(options =>
 });
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(
-        builder.Configuration.GetConnectionString("DefaultConnection")
-    ));
+   options.UseNpgsql(
+    builder.Configuration.GetConnectionString("DefaultConnection")
+));
 
 builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
