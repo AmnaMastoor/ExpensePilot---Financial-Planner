ab04af13bf1e2587d98387cb35fa58e4f7d81ff1
Amna Mastoor <amnamastoor04@gmail.com>
2026-07-22 23:20:31 +0500

    Add transaction controller and transaction APIs

---
+++ Backend/ExpensePilot.API/ExpensePilot.API/DTO/CreateTransactionDto.cs
@@ -0,0 +1,25 @@
+﻿using ExpensePilot.API.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace ExpensePilot.API.DTO
+{
+    public class CreateTransactionDto
+    {
+        
+        public int? CategoryId { get; set; }
+
+        [Required]
+        public TransactionType Type { get; set; }
+
+        [Required]
+        public decimal Amount { get; set; }
+
+        [Required]
+        public DateTime TransactionDate { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        public string? Description { get; set; }
+    }
+}
