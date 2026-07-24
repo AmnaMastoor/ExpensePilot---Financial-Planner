eff1aeab7632faea4827253264ba131966066be2
Nouman Saeed <snouman469@gmail.com>
2026-07-25 03:15:58 +0500

    Add admin dashboard endpoint

---
+++ Backend/ExpensePilot.API/ExpensePilot.API/DTO/AdminDashboardDto.cs
@@ -0,0 +1,11 @@
+﻿namespace ExpensePilot.API.DTO
+{
+    public class AdminDashboardDto
+    {
+        public int TotalUsers { get; set; }
+        public int TotalTransactions { get; set; }
+        public int TotalCategories { get; set; }
+        public int TotalGoals { get; set; }
+        public int TotalBudgets { get; set; }
+    }
+}
