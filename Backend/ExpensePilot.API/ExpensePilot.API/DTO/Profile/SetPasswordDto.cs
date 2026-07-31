9c6ab21ee48939ea05f89a050ceaf89cad1d13ee
Nouman Saeed <snouman469@gmail.com>
2026-07-31 22:48:06 +0500

    Implement Google password setup and add budget exceed transaction warnings

---
+++ Backend/ExpensePilot.API/ExpensePilot.API/DTO/Profile/SetPasswordDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExpensePilot.API.DTO.Profile
+{
+    public class SetPasswordDto
+    {
+        [Required]
+        public string NewPassword { get; set; }
+
+        [Required]
+        [Compare("NewPassword")]
+        public string ConfirmPassword { get; set; }
+    }
+}
