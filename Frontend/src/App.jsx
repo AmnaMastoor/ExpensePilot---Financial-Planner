69dc24d5bcef7b309e17353795e4b810b587f290
Nouman Saeed <snouman469@gmail.com>
2026-08-13 21:25:19 +0500

    Fix ThemeContext import path

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import AppRoutes from "./routes/AppRoutes";
-import { ThemeProvider } from "./context/ThemeContext";
+import { ThemeProvider } from "./Context/ThemeContext.js";
 
 function App() {
 
